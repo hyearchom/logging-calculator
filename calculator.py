a88118f0c05927d8f5ddccd997b805fc867eff74
Sirmori <askmori@protonmail.com>
2024-10-11 15:18:36 +0200

    Added setting folder
    - if folder does not exists it's automatically created
    - added unix shebang for Linux support

--- calculator.py
+++ calculator.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
+# the previous line is for unix systems and can be freely deleted
 from datetime import datetime
+import os
 
-PATH_TO_FILE = 'calculator.md'
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_PATH = os.path.join(SCRIPT_PATH, 'settings')
+# looking for necessary folder, or creating it
+if not os.path.exists(SETTINGS_PATH): os.mkdir(SETTINGS_PATH)
+
+HISTORY_PATH = os.path.join(SETTINGS_PATH, 'calculator.md')
 
 def log_entry(content, show=True):
     """writes a line into history file"""
